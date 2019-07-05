5600eb9ebb7aeb8b6185321da6d746e850df59e6
Dedi1 <dediapriyanto62@gmail.com>
2019-07-05 16:56:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anonymous/comgmaildediapriyanto62/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.anonymous.comgmaildediapriyanto62;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+public class MainActivity extends AppCompatActivity {
+    TabHost tabs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabs = (TabHost)findViewById(R.id.Data);
+        tabs.setup();
+        TabHost.TabSpec spec = tabs.newTabSpec("Tag1");
+        //Kita akan mendeklarasikan tab 1
+        spec.setContent(R.id.Data);
+        spec.setIndicator("Data");
+        tabs.addTab(spec);
+    }
+}
